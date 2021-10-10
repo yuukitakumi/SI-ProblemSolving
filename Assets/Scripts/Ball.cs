b2f06ec65f9c70c7008f9ded102b9c911b833787
Muhammad Aulia Rahman <meehchannel7@gmail.com>
2021-10-10 18:59:33 +0700

    problem4

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,25 +6,55 @@ public class Ball : MonoBehaviour
 {
     private Rigidbody2D rigidBody2D;
     public float speed;
+    private bool isPressed = false;
     public float currentSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
-        PushBall();
     }
 
     private void PushBall()
     {
-        var xForce = Random.Range(-1f, 1f);
-        var yForce = Random.Range(-1f, 1f);
-        var force = new Vector2(xForce, yForce);
-        rigidBody2D.velocity = force.normalized * speed;
+        if (isPressed) return;
+
+        var force = new Vector2(0f, 0f);
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            var north = new Vector2(0f, 1f);
+            force = north;
+            isPressed = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            var east = new Vector2(1f, 0f);
+            force = east;
+            isPressed = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            var south = new Vector2(0f, -1f);
+            force = south;
+            isPressed = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            var west = new Vector2(-1f, 0f);
+            force = west;
+            isPressed = true;
+        }
+
+        if (isPressed)
+        {
+            rigidBody2D.velocity = force.normalized * speed;
+        }
     }
 
     private void Update()
     {
         currentSpeed = rigidBody2D.velocity.magnitude;
+        PushBall();
     }
 }
